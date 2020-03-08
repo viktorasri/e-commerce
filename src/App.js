d5720589f9bf52045b8ea53770e68f9eff77fc42
Viktoras Rimkus <viktorasrimkus@Viktorass-MacBook-Pro.local>
2020-03-08 20:29:05 +0200

    add router, update menu-item with HOC and dynamic link

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 
 import HomePage from './pages/homepage/HomePage';
 import './app.scss';
 
 const App = () => {
   return (
-    <div>
-      <HomePage />
-    </div>
+    <Switch>
+      <Route exact path="/" component={HomePage} />
+    </Switch>
   );
 };
 
--- src/components/menu-item/menu-item.js
+++ src/components/menu-item/menu-item.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import './menu-item.scss';
+import { withRouter, Link } from 'react-router-dom';
 
-const menuItem = ({ title, imageUrl, size }) => {
+const menuItem = ({ title, imageUrl, size, linkUrl, match }) => {
   return (
-    <div className={`${size} menu-item`}>
+    <Link to={`${match.url}${linkUrl}`} className={`${size ? size : ''} menu-item`}>
       <div style={{ backgroundImage: `URL(${imageUrl}` }} className="background-image"></div>
       <div className="content">
         <h1 className="title">{title.toUpperCase()}</h1>
         <span className="subtitle">Shop Now</span>
       </div>
-    </div>
+    </Link>
   );
 };
 
-export default menuItem;
+export default withRouter(menuItem);
