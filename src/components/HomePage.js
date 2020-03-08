15699d0f48ce541c7ae4d609d6d199da446192ca
Viktoras Rimkus <viktorasrimkus@Viktorass-MacBook-Pro.local>
2020-03-08 11:43:32 +0200

    initial project setup with homepage

---
+++ src/components/HomePage.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './homepage.scss';
+
+const homePage = () => {
+  return (
+    <div className="homepage">
+      <div className="directory-menu">
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="title">Hats</h1>
+            <span className="subtitle"></span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default homePage;
