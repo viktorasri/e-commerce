fc00f3a06a123d442ad183f097c8ac6123900fbb
Viktoras Rimkus <viktorasrimkus@Viktorass-MacBook-Pro.local>
2020-04-01 14:53:45 +0300

    fix node server path bug

--- server.js
+++ server.js
@@ -15,11 +15,13 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path(__dirname, 'client/build')));
+  if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, 'client/build')));
 
-  app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-  });
+    app.get('*', function(req, res) {
+      res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+    });
+  }
 }
 
 app.listen(port, error => {
